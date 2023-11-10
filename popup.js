04402532710ac9bbff560dbd0d02c30acc72df4d
Jake Camarao <jcamarao@hawaii.edu>
2023-11-09 14:58:59 -1000

    remove toggle

--- popup.js
+++ popup.js
@@ -64,16 +64,6 @@ document.getElementById("expansionSelect").addEventListener("change", function()
     browser.storage.local.set({"expansion": expansionSelect.value})
 })
 
-/* obtain and update the value if it's day/night based on the 6/6 EZT time cycle
- * day: 6am-5:59pm
- * night: 6pm - 5:59am
- */
- document.getElementById("dayNightToggle").addEventListener("change", function(event) {
-    browser.storage.local.get("expansion", (item) => {
-        document.getElementById("dayNightToggle").checked = item.value
-    })
-})
-
 /* set the toggle accordingly to current time when opening pop-up */
 browser.storage.local.get("isNight", (item) => {
     item.isNight ? document.getElementById("dayNightToggle").checked = true : document.getElementById("dayNightToggle").checked = false
